bba77970d0b7e3ed2832616d52bc68d685e1deb5
masqomar21 <mqomarudin0480@gmail.com>
2024-11-29 23:53:20 +0700

    feat: add logic carousel

--- resources/js/Components/Slider.tsx
+++ resources/js/Components/Slider.tsx
@@ -1,6 +1,8 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { motion, AnimatePresence } from 'framer-motion'
 import { wrap } from 'popmotion'
+import PrevIcon from './Icon/PrevIcon'
+import NextIcon from './Icon/NextIcon'
 
 const variants = {
     enter: (direction: number) => ({
@@ -26,9 +28,17 @@ const swipePower = (offset: number, velocity: number) => {
 
 interface SliderProps {
     children: React.ReactNode[]
+    className?: String
+    interval?: number
+    autoSlider?: boolean
 }
 
-export const Slider: React.FC<SliderProps> = ({ children }) => {
+export const Slider: React.FC<SliderProps> = ({
+    children,
+    className,
+    interval,
+    autoSlider = true
+}) => {
     const [[page, direction], setPage] = useState<[number, number]>([0, 0])
 
     const childIndex = wrap(0, children.length, page)
@@ -37,9 +47,20 @@ export const Slider: React.FC<SliderProps> = ({ children }) => {
         console.log('newDirection', newDirection)
         setPage([page + newDirection, newDirection])
     }
+    useEffect(() => {
+        if (autoSlider) {
+            const autoSlide = setInterval(() => {
+                paginate(1)
+            }, interval || 3000)
+
+            return () => clearInterval(autoSlide)
+        }
+    }, [childIndex])
 
     return (
-        <div className="relative z-10 w-full h-full overflow-hidden bg-gray-100">
+        <div
+            className={`relative z-10 w-full h-full overflow-hidden bg-gray-100 ${className}`}
+        >
             <AnimatePresence initial={false} custom={direction}>
                 <motion.div
                     key={page}
@@ -74,13 +95,24 @@ export const Slider: React.FC<SliderProps> = ({ children }) => {
                 className="absolute z-20 top-1/2 left-4 transform -translate-y-1/2 bg-white p-2 rounded-full shadow-md hover:bg-gray-200 focus:outline-none"
                 onClick={() => paginate(-1)}
             >
-                {'❮'}
+                <PrevIcon className="justify-center flex" size={24} />
             </div>
             <div
                 className="absolute z-20 top-1/2 right-4 transform -translate-y-1/2 bg-white p-2 rounded-full shadow-md hover:bg-gray-200 focus:outline-none"
                 onClick={() => paginate(1)}
             >
-                {'❯'}
+                <NextIcon className="justify-center flex" size={24} />
+            </div>
+            <div className="absolute z-20 bottom-2 left-1/2 transform -translate-x-1/2 flex gap-2">
+                {Array.from({ length: children.length }).map((_, index) => (
+                    <button
+                        key={index}
+                        onClick={() => setPage([index, index - childIndex])}
+                        className={`w-3 h-3 rounded-full cursor-pointer ${
+                            index === childIndex ? 'bg-gray-500' : 'bg-gray-200'
+                        }`}
+                    ></button>
+                ))}
             </div>
         </div>
     )
--- resources/js/Pages/Welcome.tsx
+++ resources/js/Pages/Welcome.tsx
@@ -9,6 +9,19 @@ type WelcomeProps = {
     name: string
 }
 
+const berita = [
+    {
+        title: 'Gelar Lmbka Karya Studio Setingkat Nasional',
+        date: '2021-10-10',
+        image: './assets/img/test.png'
+    },
+    {
+        title: 'Staf Pengajar Prodi Desain Komunikasi Visual Raih Penghargaan',
+        date: '2021-10-10',
+        image: './assets/img/test.png'
+    }
+]
+
 export default function Welcome({ name }: WelcomeProps) {
     const visitor = useVisitor()
     const image = '/assets/img/logo.png'
@@ -33,7 +46,7 @@ export default function Welcome({ name }: WelcomeProps) {
     }
     return (
         <AppLayout title={'welcome'}>
-            <div className="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50">
+            {/* <div className="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50">
                 <p className="text-center text"> Wlecome </p>
                 <p> {name} test </p>
                 <img src={image} alt="test" />
@@ -43,36 +56,33 @@ export default function Welcome({ name }: WelcomeProps) {
                 <Link href={route('berita')}>
                     {visitor.visitorCount} {JSON.stringify(visitor.visitorData)}
                 </Link>
-            </div>
-            <div
-                style={{
-                    display: 'flex',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    height: '100vh'
-                }}
-            >
-                <motion.h1
-                    initial={{ opacity: 0, x: -100 }} // Menentukan keadaan awal (transparansi 0 dan bergerak ke kiri)
-                    animate={{ opacity: 1, x: 0 }} // Menentukan keadaan akhir (transparansi penuh dan berada di tengah)
-                    transition={{ duration: 1 }} // Durasi animasi
-                    className="text-black"
-                >
-                    Selamat Datang di Framer Motion!
-                </motion.h1>
-            </div>
+            </div> */}
 
-            <div className="h-screen flex items-center justify-center bg-gray-50">
-                <Slider>
-                    <div className="w-full h-64 flex items-center justify-center bg-red-400 text-white text-2xl font-bold">
+            <div className="aspect-video flex items-center justify-center bg-gray-50">
+                <Slider autoSlider={false}>
+                    {berita.map((item, index) => (
+                        <div
+                            className={`relative w-full h-full  bg-cover bg-center`}
+                            style={{ backgroundImage: `url(${item.image})` }}
+                        >
+                            <div className="absolute w-full h-full flex items-center justify-center bg-gradient-to-r from-[#236899] to-transparent">
+                                <h1 className="text-white text-4xl font-bold">
+                                    {item.title}
+                                </h1>
+                            </div>
+                            {/* <img src={item.image} alt="test" /> */}
+                            {/* {item.title} */}
+                        </div>
+                    ))}
+                    {/* <div className="w-full h-full flex items-center justify-center bg-red-400 text-white text-2xl font-bold">
                         Slide 1
                     </div>
-                    <div className="w-full h-64 flex items-center justify-center bg-blue-400 text-white text-2xl font-bold">
+                    <div className="w-full h-full flex items-center justify-center bg-blue-400 text-white text-2xl font-bold">
                         Slide 2
                     </div>
-                    <div className="w-full h-64 flex items-center justify-center bg-green-400 text-white text-2xl font-bold">
+                    <div className="w-full h-full flex items-center justify-center bg-green-400 text-white text-2xl font-bold">
                         Slide 3
-                    </div>
+                    </div> */}
                 </Slider>
             </div>
         </AppLayout>
