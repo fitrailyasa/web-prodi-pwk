172ed382e1390d84a86319b90846c418f0e38620
Fitra Ilyasa <85192292+fitrailyasa@users.noreply.github.com>
2025-05-25 10:23:41 +0700

    add new column layanans

--- app/Http/Requests/LayananRequest.php
+++ app/Http/Requests/LayananRequest.php
@@ -23,15 +23,11 @@ class LayananRequest extends FormRequest
     {
         $rules = [
             'name' => 'required|max:100',
+            'file' => 'nullable|file|mimes:pdf,doc,docx|max:10240',
+            'link' => 'nullable|max:100',
+            'linkType' => 'required|max:100',
+            'type' => 'required|max:100',
         ];
-
-        // Jika method POST (create), file wajib
-        if ($this->method() === 'POST') {
-            $rules['file'] = 'required|file|mimes:pdf,doc,docx|max:10240';
-        } else {
-            // Jika method PUT (update), file tidak wajib
-            $rules['file'] = 'nullable|file|mimes:pdf,doc,docx|max:10240';
-        }
 
         return $rules;
     }
@@ -41,9 +37,14 @@ class LayananRequest extends FormRequest
         return [
             'name.required' => 'Nama Layanan tidak boleh kosong!',
             'name.max' => 'Nama Layanan maksimal 100 karakter!',
-            'file.required' => 'File Layanan harus diupload!',
             'file.mimes' => 'File Layanan harus berupa PDF, DOC, atau DOCX!',
             'file.max' => 'File Layanan maksimal 10MB!',
+            'link.required' => 'Link Layanan tidak boleh kosong!',
+            'link.max' => 'Link Layanan maksimal 100 karakter!',
+            'linkType.required' => 'Tipe Link Layanan tidak boleh kosong!',
+            'linkType.max' => 'Tipe Link Layanan maksimal 100 karakter!',
+            'type.required' => 'Tipe Layanan tidak boleh kosong!',
+            'type.max' => 'Tipe Layanan maksimal 100 karakter!',
         ];
     }
 }
